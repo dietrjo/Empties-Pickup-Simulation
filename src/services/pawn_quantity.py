37aac4fca6df1f21039fdd73a0c3c19cd7f2f960
Nils <120587350+OnlyNils@users.noreply.github.com>
2024-12-26 18:58:55 +0100

    added pawn_quantity prediction

---
+++ src/services/pawn_quantity.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def get_pawn_quanity(expectation_value=16, standard_deviation=10) -> int:
+    """
+    Gibt einen Zufallswert x zurück, basierend auf einer abgeschnittenen Normalverteilung,
+    die nur Werte x >= 0 berücksichtigt
+    
+    Parameters:
+        expectation_value: Erwartungswert µ der Normalverteilung
+        standard_deviation: Standardabweichung σ der Normalverteilung
+
+    Returns:
+        int: Ein Zufallswert x, gerundet auf die nächste ganze Zahl, immer >= 0.
+    """
+    while True:
+        # Generiere einen Wert basierend auf der Normalverteilung
+        value = np.random.normal(expectation_value, standard_deviation)
+        
+        # Wenn der Wert >= 0 ist, gib ihn zurück
+        if value >= 0:
+            return round(value)
+
+
+
+if __name__ == '__main__':
+    print(get_pawn_quanity())
