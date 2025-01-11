5d5cc41753691d6bbe283a3e802ff1e00204d779
dietrjo <dietrich.jonas04@gmail.com>
2025-01-11 21:53:25 +0100

    strategy 2 is now also implemented

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from src.services.location import get_customer_locations, get_warehouse_location
 from src.services.map import create_map
 from src.services.tour import get_tours, get_routes_lat_lons
 from src.strategies.strategy1 import simulate_strategy_1
+from src.strategies.strategy2 import simulate_strategy_2
 
 
 def main():
@@ -14,6 +15,7 @@ def main():
     routes_lat_lons = get_routes_lat_lons(tours, coords)
 
     simulate_strategy_1(100000, tours)
+    simulate_strategy_2(100000, tours)
 
     create_map(warehouse_location, customer_locations, routes_lat_lons).save("tour_map.html")
     webbrowser.open("tour_map.html")
--- src/strategies/strategy2.py
+++ src/strategies/strategy2.py
@@ -1,11 +1,12 @@
-# Strategy 1: Pickup as many liters of empties at each address as will be delivered to the customer with the order.
+# Strategy 2: Pickup as many liters of empties at each address as will fit in the delivery vehicle.
 
+from src.services.random_quantity import get_normal_distribution_quantity
 from src.utils.data_loader import load_json
 
 data = load_json('data.json')
 
 
-def simulate_strategy_1(n: int, tours: list[list[int]]):
+def simulate_strategy_2(n: int, tours: list[list[int]]):
     vehicle_capacities = data["vehicle_capacities"]
     demands = data["customers"]["demands"]
 
@@ -30,7 +31,22 @@ def simulate_strategy_1(n: int, tours: list[list[int]]):
 
             vehicle_space = vehicle_capacity - tour_demand
 
+            for j in range(1, len(tour) - 1):
+                demand = demands[tour[j]]
+                pickup_demand = get_normal_distribution_quantity(5)
+
+                pickup_demands += pickup_demand
+                vehicle_space += demand
+
+                if vehicle_space >= pickup_demand:
+                    pickup_quantity += pickup_demand
+                    vehicle_space -= pickup_demand
+                else:
+                    pickup_quantity += vehicle_space
+                    vehicle_space = 0
+
         fulfillment_level = pickup_quantity / pickup_demands
         sum_fulfillment_level += fulfillment_level
 
     average_fulfillment_level = sum_fulfillment_level / n
+    print(f'Erfüllungsgrad von Strategie 2: {round(average_fulfillment_level * 100, 2)}%')
